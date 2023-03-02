3b9a1b563f4d87832c393b90d7d80183e5c21500
jbucko <jbucko@ethz.ch>
2023-03-02 14:54:22 +0100

    one-body decaying DM added

--- src/DarkMatterEmulators.py
+++ src/DarkMatterEmulators.py
@@ -7,6 +7,79 @@ sys.path.append(os.path.dirname(__file__))
 from my_lib_colab5 import *
 from typing import Union
 
+
+class OBDemu():
+    """
+    One body decaying dark matter emulator, as developed and described in 2104.07675 (10.1088/1475-7516/2021/10/040)
+    """
+    def __init__(self):
+        print('One-body decays emulator loaded!')
+
+    def predict(self, k: Union[float,list], z: Union[float,list], Gamma: float = 1e-10, f: float = 0., Ob: float =0.049, Om: float = 0.315, h:float = 0.67):
+        """
+        Enqvist et al. 2015 calibrated on simulations by Jonathan Hubert & Aurel Schneider
+        :param k: scale in h/Mpc
+        :param z: redshift
+        :param Gamma: decay rate of dark matter in 1/Gyr
+        :param f: fraction of decaying dark matter in total dark matter budget
+        :param Ob: total matter abundance
+        :param Om: total matter abundance
+        :param h: present value of hubble constant
+        :return: ratio of nonlinear matter power spectra of decaying DM and LCDM
+        """
+
+        # convert big omegas (input) to small omegas (used for the fit)
+        wb = Ob*h*h
+        wm = Om*h*h
+
+        # convert k from h/Mpc to 1/Mpc
+        k = k*h
+
+        # Run some checks
+        assert f >= 0. and f <= 1., "f is not within (0,1), chosen f is: {}".format(f) # well-defined f
+        assert Gamma >= 0, "Gamma is not positive, chosen Gamma is: {}".format(Gamma)
+        for i in range(len(k)):
+            assert k[i] <= 10 and k[i] >= 1e-6, "found k value not in good range (1e-6,10) [1/Mpc]. Value is {}".format(k[i])
+        assert z >= 0, "z is not positive, chosen z is: {}".format(z)
+
+        # These are only to check if the parameter is in a range where low error is expected.
+        # Fit can still function well outside this range if the values are not extreme
+        if(z.any() > 1): # We know the fitting function is more accurate for lower redshifts < 1
+            print("You have chosen a higher redshift z={}!\n-> the fit could be unaccurate with this choice! (error might be > 10%)".format(z))
+        if(Gamma >= 0.0316): # We know the fitting function is more accurate for higher lifetimes > 31.6 Gyr
+            print("You have chosen a short lifetime of {} Gyr!\n-> the fit could be unaccurate with this choice! (error might be > 10%)".format(Gamma**-1.))
+        if(wb< 0.019 or wb > 0.026):
+            print("You have chosen wb={}!\n-> the fit could be unaccurate with this choice! (error might be > 10%)".format(wb))
+        if(wm< 0.09 or wm > 0.28):
+            print("You have chosen wm={}!\n-> the fit could be unaccurate with this choice! (error might be > 10%)".format(wm))
+        if(h< 0.6 or h > 0.8):
+            print("You have chosen h={}!\n-> the fit could be unaccurate with this choice! (error might be > 10%)".format(h))
+
+        # Fit calculation, notice that we need k in h/Mpc for the calculation (fit is built upon sims with h/Mpc)
+        # k = k*h
+
+        a = 0.7208 + 2.027*Gamma + (3.431 - 0.4)*(1./(1.+z*1.1)) - 0.18
+        b = 0.0120 + 2.786*Gamma + (0.6499 + 0.02)*(1./(1.+z*1.1)) - 0.09
+        p = 1.045 + 1.225*Gamma + (0.2207)*(1./(1.+z*1.1)) - 0.099
+        q = 0.9922 + 1.735*Gamma + (0.2154)*(1./(1.+z*1.1)) - 0.056
+
+        u = wb/0.02216
+        v = h/0.6776
+        w = wm/0.1412
+        
+        eps1 = 5.323 - 1.4644*u - 1.391*v + (-2.055 +1.329*u + 0.8672*v)*w + (0.2682 - 0.3509*u)*w*w
+        eps2 = 0.9260 + (0.05735 - 0.02690*v)*w + (-0.01373 + 0.006713*v)*w*w
+        eps3 = (9.553 - 0.7860*v) + (0.4884 + 0.1754*v)*w + (-0.2512 + 0.07558*v)*w*w
+
+        linear_fit = eps1*((Gamma)**eps2)*((1./(1.+z*0.105))**eps3)
+        nonlinear_fit = (1.+a*(k**p))/(1.+b*(k**q))*(f)
+
+        # print('k-shape,z-shape,PP-shape',k.shape,z.shape,(-linear_fit*nonlinear_fit + 1.).shape)
+        # print('a,b,p,q,eps1,eps2,eps3,linear fit, nonlinear fit:',a.shape,b.shape,p.shape,q.shape,eps1.shape,eps2.shape,eps3.shape,linear_fit.shape,nonlinear_fit.shape)
+        # print('z,k:',z[:20],k[:20])
+        
+        return -linear_fit*nonlinear_fit + 1.
+
 class TBDemu():
     def __init__(self):
         PATH_TO_EMULATOR = os.path.dirname(__file__) + '/files/'
